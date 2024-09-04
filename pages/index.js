3c33e34add1a4171c8b247fa88aa08add8c68435
Vinicius Hayden <67245806+vinicius-hayden@users.noreply.github.com>
2024-09-04 22:31:15 +0000

    under construction message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Fala Pai! Apesar dos apesares e sempre poder me empenhar mais, vou continuar tentando dar meu melhor! </h1>
+    return <h1> Under Construction </h1>
 }
 
 export default Home;
