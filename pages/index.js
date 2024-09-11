418b4a66269ba2d6be08010e62592527625ab026
vinicius-hayden <vinihaydenss@gmail.com>
2024-09-11 16:30:30 -0400

    typos and class indenting

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,28 @@ export default function Home() {
     <>
       <div className="main">
         <div className="main-quote">
-          <p className="main-phrase">
-            "It is not because things are difficult that we do not dare, but
-            because we do not dare that they are difficult."
-            <p className="main-author">Seneca</p>
-            <div className="main-purpose">
-              I'm still not sure what to build with this project, I absoloutely,
-              however, want to apply every little concept I grasp from the
-              course, I'm willing my best to showcase my father how much
-              <br />
-              I've been learning so far. It is not about the goal, but the
-              journey. It might look big and impossible to reach, but it turns
-              out it breaks down to this very moment. Memento mori!
+          <div className="quote-photo">
+            <div className="main-quote-author">
+              <p className="main-phrase">
+                "It is not because things are difficult that we do not dare, but
+                because we do not dare that they are difficult."
+              </p>
+              <p className="main-author">- Seneca</p>
+              <div className="main-quote-purpose">
+                <p className="main-purpose">
+                  I'm still not sure what to build with this project, I
+                  absolutely, however, want to apply every little concept I
+                  grasp from the course, I'm willing to do my best to showcase
+                  my father how much
+                  <br />
+                  I've been learning so far. It is not about the goal, but the
+                  journey. It might look big and impossible to reach, but it
+                  turns out it breaks down to this very moment. Memento mori!
+                </p>
+              </div>
             </div>
-          </p>
-
-          <div className="main-image-quote"></div>
+            <div className="main-image-quote" />
+          </div>
         </div>
       </div>
     </>
