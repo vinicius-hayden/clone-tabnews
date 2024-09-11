a0a85618589293b7add409b5cc82375a116ca37e
vinicius-hayden <vinihaydenss@gmail.com>
2024-09-11 11:56:30 -0400

    initial homepage ideas

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-function Home() {
-  return <h1> Under Construction! </h1>;
+export default function Home() {
+  return (
+    <>
+      <div className="main">
+        <div className="main-quote">
+          <p className="main-phrase">
+            "It is not because things are difßficult that we do not dare, but
+            because we do not dare that they are difficult."
+          </p>
+          <p className="main-author">Seneca</p>
+          <div className="main-image-quote"></div>
+        </div>
+        <div className="main-purpose">
+          I'm still not sure what to build with this project, I absoloutely,
+          however, want to apply every little concept I grasp from the course,
+          I'm willing my best to showcase my father how much I've been learning
+          so far. It is not about the goal, but the journey. It might look big
+          and impossible to reach, but it turns out it breaks down to this very
+          moment. Memento mori!
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default Home;
