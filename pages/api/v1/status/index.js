baedaf314c891311a2acf570bd4c8329bbd92a65
vinicius-hayden <vinihaydenss@gmail.com>
2024-09-19 19:03:13 -0400

    implementing database status return with connection monitoring

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1');
-  console.log(result.rows);
-  response.status(200).json({message: "Deschamps é muito massa!"});
+  const updatedAt = new Date().toISOString()
+
+  let queryVersion = await database.query('SELECT version()');
+  const postgresVersion = queryVersion.rows[0].version;
+
+  let queryMaximumConnections = await database.query('SHOW max_connections')
+  const maximumConnections = queryMaximumConnections.rows[0].max_connections;
+
+  let queryUsedConnections = await database.query(`SELECT * FROM pg_stat_activity WHERE datname = 'local_db';`)
+  const usedConnections = queryUsedConnections.rows;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: postgresVersion,
+    maximum_connections: maximumConnections,
+    used_connections: usedConnections,
+  });
 }
 
 export default status;
