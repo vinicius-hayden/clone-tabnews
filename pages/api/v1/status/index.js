7c66c375734d58cb6ad1c9745700f47ccab8c3d8
vinicius-hayden <vinihaydenss@gmail.com>
2024-09-20 14:24:15 -0400

    adding `dependencies`, `database` thread and correct names

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,22 +1,30 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString()
+  const updatedAt = new Date().toISOString();
 
-  let queryVersion = await database.query('SELECT version()');
-  const postgresVersion = queryVersion.rows[0].version;
+  let queryVersion = await database.query("SHOW server_version;");
+  const postgresVersion = parseInt(queryVersion.rows[0].server_version);
 
-  let queryMaximumConnections = await database.query('SHOW max_connections')
-  const maximumConnections = queryMaximumConnections.rows[0].max_connections;
+  let queryMaximumConnections = await database.query("SHOW max_connections;");
+  const maximumConnections = parseInt(
+    queryMaximumConnections.rows[0].max_connections,
+  );
 
-  let queryUsedConnections = await database.query(`SELECT * FROM pg_stat_activity WHERE datname = 'local_db';`)
-  const usedConnections = queryUsedConnections.rows;
+  let queryUsedConnections = await database.query(
+    `SELECT * FROM pg_stat_activity WHERE datname = 'local_db';`,
+  );
+  const usedConnections = queryUsedConnections.rows.length;
 
   response.status(200).json({
     updated_at: updatedAt,
-    postgres_version: postgresVersion,
-    maximum_connections: maximumConnections,
-    used_connections: usedConnections,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: maximumConnections,
+        opened_connections: usedConnections,
+      },
+    },
   });
 }
 
