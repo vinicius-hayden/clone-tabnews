332fe4328193448eaa3582202277b367318fe354
vinicius-hayden <vinihaydenss@gmail.com>
2024-09-20 14:24:39 -0400

    renaming api pathway

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -8,12 +8,11 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.postgres_version).toBe('PostgreSQL 16.0 on aarch64-unknown-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit');
+  expect(responseBody.dependencies.database.version).toBe(16.0);
   
-  const parsedMaximumConnections = parseInt(responseBody.maximum_connections);
+  const parsedMaximumConnections = parseInt(responseBody.dependencies.database.max_connections);
   expect(parsedMaximumConnections).toBeGreaterThan(1);
 
-  const usedConnectionsQuantity = responseBody.used_connections.length;
-  expect(usedConnectionsQuantity).toBeGreaterThan(0);
+  expect(responseBody.dependencies.database.opened_connections).toBeGreaterThan(0);
   
 })
