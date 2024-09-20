9351f8d4e02ed2023b5bd7930f42c98984b48f5f
vinicius-hayden <vinihaydenss@gmail.com>
2024-09-20 14:53:54 -0400

    fixed pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,10 +9,7 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
   expect(responseBody.dependencies.database.version).toBe(16.0);
-  
-  const parsedMaximumConnections = parseInt(responseBody.dependencies.database.max_connections);
-  expect(parsedMaximumConnections).toBeGreaterThan(1);
-
-  expect(responseBody.dependencies.database.opened_connections).toBeGreaterThan(0);
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
   
 })
