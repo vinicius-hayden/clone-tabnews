e6017384e071784fcefc15d202176b11f15c670c
vinicius-hayden <vinihaydenss@gmail.com>
2024-09-20 14:50:30 -0400

    changing `maximumConnections` to `maxConnections` and `usedConnections` to `openedConnections`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,23 +6,23 @@ async function status(request, response) {
   let queryVersion = await database.query("SHOW server_version;");
   const postgresVersion = parseInt(queryVersion.rows[0].server_version);
 
-  let queryMaximumConnections = await database.query("SHOW max_connections;");
-  const maximumConnections = parseInt(
-    queryMaximumConnections.rows[0].max_connections,
+  let queryMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(
+    queryMaxConnections.rows[0].max_connections,
   );
 
   let queryUsedConnections = await database.query(
     `SELECT * FROM pg_stat_activity WHERE datname = 'local_db';`,
   );
-  const usedConnections = queryUsedConnections.rows.length;
+  const openedConnections = queryUsedConnections.rows.length;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: postgresVersion,
-        max_connections: maximumConnections,
-        opened_connections: usedConnections,
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
       },
     },
   });
