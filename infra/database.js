da9252db225716809b42922c8b35a7d5eaaf123c
vinicius-hayden <vinihaydenss@gmail.com>
2024-09-23 13:27:45 -0400

    setting up remote database safely

--- infra/database.js
+++ infra/database.js
@@ -8,14 +8,7 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  console.log('Postgres Credentials', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  })
-  
+    
   try {
     await client.connect();
     const result = await client.query(queryObject);
