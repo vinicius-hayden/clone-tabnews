5863ece94ceba3ed73c81dfeec7c316d67967881
vinicius-hayden <vinihaydenss@gmail.com>
2024-09-27 01:30:54 -0400

    enhanced local migration post method check

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,13 +2,33 @@ import migrationRunner from 'node-pg-migrate'
 import { join } from "node:path"
 
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
+  const defaultMigrationOptions = {
     databaseUrl: process.env.DATABASE_URL,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
     verbose: true,
     migrationsTable: "pgmigrations",
-  });
-  response.status(200).json(migrations);
+  };
+  
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
+  
 }
