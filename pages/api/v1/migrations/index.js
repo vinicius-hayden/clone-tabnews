98f858d226041903b394a0e71a1585915f3ecb22
Vinicius Hayden <67245806+vinicius-hayden@users.noreply.github.com>
2024-10-23 00:07:41 +0000

    fix: adjust `node-pg-migrate` to check deployment

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
+import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
@@ -18,7 +18,7 @@ export default async function migrations(request, response) {
     const defaultMigrationOptions = {
       dbClient: dbClient,
       dryRun: true,
-      dir: resolve("infra", "migrations"),
+      dir: join("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
